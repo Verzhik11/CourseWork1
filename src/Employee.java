495dc63af444e3bf52c03f8ea9281603cdf718e0
Verzhik <verzhik11@gmail.com>
2023-02-11 12:01:24 +0300

    Курсовая работ выполнена. Базовый уровень.

--- src/Employee.java
+++ src/Employee.java
@@ -3,25 +3,25 @@ public class Employee {
     private int salary;
     private final int department;
     private int id;
+    private static int counter = 1;
 
 
-    public Employee(String name, int salary, int department, int id) {
+    public Employee(String name, int salary, int department) {
         this.name = name;
         this.salary = salary;
-        this.department = department;
-        this.id = id;
-        id = idCounter++;
+        this.department = checkDepartment(department);
+        this.id = counter++;
     }
-    public static int idCounter = 0;
-
-
-    public int checkDepartment() {
+    public int checkDepartment(int department) {
         if (department >= 1 && department <= 5) {
             return department;
         } else {
             throw new IllegalArgumentException("Ошибка при написании отдела");
-
         }
+    }
+
+    public String toString() {
+        return "Работник" + " " + this.name + " работает в отделе " + this.department + " и получает ЗП " + this.salary + " рублей";
     }
 
     public String getName() {
@@ -40,4 +40,7 @@ public class Employee {
         return department;
     }
 
+    public int getId() {
+        return id;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,62 @@
 public class Main {
-    private final Employee [] employees;
-    public Main () {
-        this.employees = new Employee[10];
-    }
+    private static final Employee[] employees = new Employee[10];
 
     public static void main(String[] args) {
-        Employee 1 = new Employee (" Максим Ласточкин",54000, 2, );
+        employees[0] = new Employee("Максим Ласточкин", 54000, 2);
+        employees[1] = new Employee("Олег Иванов", 75890, 1);
+        employees[2] = new Employee("Корней Свиридов", 125350, 3);
+        employees[3] = new Employee("Леонид Семенов", 84100, 1);
+        employees[4] = new Employee("Матвей Ерохов", 158000, 4);
+        employees[5] = new Employee("Игнат Шалимов", 58900, 2);
+        employees[6] = new Employee("Арсений Климов", 245000, 5);
+        employees[7] = new Employee("Евгения Румянцева", 95000, 3);
+        employees[8] = new Employee("Кристина Солженицына", 69800, 1);
+        employees[9] = new Employee("Алина Костарева", 49000, 2);
+        printEmployees();
+        countTotalSalary();
+        printMaxSalary();
+        printMinSalary();
+        printNameWithId();
+        }
+
+    public static void printEmployees() {
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println(employees[i]);
+        }
     }
+    public static void countTotalSalary () {
+        int totalSalary = 0;
+        for (int i = 0; i < employees.length; i++) {
+            totalSalary = totalSalary + employees[i].getSalary();
+        }
+        System.out.println("Общая сумма выплаты ЗП сотрудникам равна " + totalSalary + " рублей");
+        System.out.println("Средняя ЗП составляет " + totalSalary / employees.length + " рублей");
+    }
+
+    public static void printMinSalary() {
+        double minSalary = 1.0 / 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getSalary() < minSalary) {
+                minSalary = employees[i].getSalary();
+            }
+        }
+        System.out.println("Минимальная ЗП среди сотрудников составляет " + minSalary + " рублей" );
+    }
+    public static void printMaxSalary() {
+        int maxSalary = -1;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getSalary() > maxSalary) {
+                maxSalary = employees[i].getSalary();
+            }
+        }
+        System.out.println("Максимальная ЗП среди сотрудников составляет " + maxSalary + " рублей" );
+    }
+
+    public static void printNameWithId() {
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println(employees[i].getId() + " " + employees[i].getName());
+
+        }
+    }
+
 }
